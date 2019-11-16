41e253741c370c00413439bec1c3e292b5616e7d
mindeby <debora.marisa.ribeiro@gmail.com>
2019-11-16 19:06:15 +0100

    got started with basic elements

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('#other-title').hide(); //show this option only when user selects 'other' option
+$(':input:visible:first').focus(); // focus the first field
+
+
+
+$('#color')[0].options[0].innerText = "Please select a T-shirt theme"
+
+$('#color')[0].options;
+
+let test = $('#color:first-of-type')
+
+console.log(test)
