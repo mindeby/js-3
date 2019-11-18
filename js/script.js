5956e9481cd4520e977426461d57a0849e8835c5
mindeby <debora.marisa.ribeiro@gmail.com>
2019-11-18 17:19:45 +0100

    progress with color/design section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
 $('#other-title').hide(); //show this option only when user selects 'other' option
 $(':input:visible:first').focus(); // focus the first field
+$('#color').children().hide(); //hide the options
+const selectTheme = document.createElement("OPTION"); //create a new option
+$('#color').prepend(selectTheme)  //prepend it to the option list
+selectTheme.innerText = "Please select a T-shirt theme" //ask the user to select a design first
+selectTheme.selected = "selected" //set it to selected
 
+let themes = [
+  {
+    name: "js puns",
+    colors: ["cornflowerblue", "darkslategrey", "gold"]
+  },
 
+  {
+    name: "heart js",
+    colors: ["tomato", "steelblue", "dimgrey"]
+  }
+]
 
-$('#color')[0].options[0].innerText = "Please select a T-shirt theme"
+$('#design').change(function(){
+  $('#color').children().hide();  //hide all options again
+  availableColors = [];
+  let selectedDesign = $('#design').val(); //design the user chose
+  $.each( themes, function( i, val ) { //check themes
+    if(themes[i].name === selectedDesign) { //if the selection matches the theme name
+      availableColors = themes[i].colors; //tell me which colors are available for that theme
+    }
+  })
 
-$('#color')[0].options;
-
-let test = $('#color:first-of-type')
-
-console.log(test)
+  for (i=0; i < $('#color').children().length; i += 1 ) { //check all the options
+    let option = $('#color').children()[i].value; //give me their values
+    if (availableColors.indexOf(option) !== -1)  { //if the values are on the list of available colors
+      $('#color').children()[i].style.display = "block" //display that option on the color selector
+    }
+  }
+});
