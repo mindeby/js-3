5f049a096f7ecd69d53c4ed1458d6e8c3f48fcc1
mindeby <debora.marisa.ribeiro@gmail.com>
2019-11-19 11:53:03 +0100

    working on box unchecking

--- js/script.js
+++ js/script.js
@@ -75,33 +75,37 @@ console.log(events)
 
 function checkConflict(eventA,eventB) {
   if (eventA.day === eventB.day) { //if the activity is on the same day
-    if (eventA.startTime >= eventB.startTime && eventA.startTime <= eventB.endTime  ) { //aand starts at the same time
+    if (eventA.startTime >= eventB.startTime && eventA.startTime <= eventB.endTime  ) { //and starts at the same time
       eventB.isConficting = true;
     }
   }
 }
 
 $('input:checkbox').change(function(event){
-  if($(this).is(':checked')){
-    let checkedBox = $(this)[0].name; //get me the name attribute of the checkbox that was clicked
-    $.each( events, function( i, val ) {
-      if (checkedBox === val.name){ //if it matches the name of one of the events object
-        let eventA = events[i] //give me the index and set it to EventA
-        $.each( events, function( i, val ) {
-          if ( eventA.name !== val.name) { //If it's not the same event
-            checkConflict(eventA, val) //check for scheduling conflicts
-          }
-        });
-      }
-    });
-  }
+  //if($(this).is(':checked')){
+  let checkedBox = $(this)[0].name; //get me the name attribute of the checkbox that was checked or unchecked
   $.each( events, function( i, val ) {
+    if (checkedBox === val.name){ //if it matches the name of one of the events object
+      let eventA = events[i] //give me the index and set it to EventA
+      $.each( events, function( i, val ) {
+        if ( eventA.name !== val.name) { //If it's not the same event
+          checkConflict(eventA, val) //check for scheduling conflicts
+          console.log('im checking for conflicts')
+        }
+      });
+    }
+  });
+  //}
+  $.each( events, function( i, val ) {
+    let conflictingLabel = fieldset_activities$.find("label")[i];
     if (val.isConficting) {
-
-      let conflictingLabel = fieldset_activities$.find("label")[i];
       activities[i].disabled = true; //disabled conflicting activities
       conflictingLabel.style.color="grey"
       conflictingLabel.style.textDecoration="line-through"
+    } else {
+      activities[i].disabled = false; //disabled conflicting activities
+      conflictingLabel.style.color="black"
+      conflictingLabel.style.textDecoration="none"
     }
   });
 });
